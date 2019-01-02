ddfe203416a35fa89e2630a6a1dcb8598a7c20c9
JoshuaMoratalla <joshuamoratalla87@gmail.com>
2019-01-02 11:19:21 +1000

    Initial login

---
+++ Market_Watch_Solo/Market_Watch_Solo/Market_Watch_Solo/Action/Login_Check.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Market_Watch_Solo.Model;
+using System.Net;
+using Newtonsoft.Json;
+using SimpleJson;
+using RestSharp;
+using RestSharp.Deserializers;
+using RestSharp.Serialization.Json;
+
+namespace Market_Watch_Solo.Action
+{
+    class Login_Check
+    { 
+        public User UserResult { get; set; }
+        public string Input_Username { get; set; }
+        public string Input_Password { get; set; }
+        public bool Validated { get; set; }
+        public string URL { get; set; }
+
+        public Login_Check(string inEmail, string inPass)
+        {
+            Input_Username = inEmail;
+            Input_Password = inPass;
+            Validated = false;
+
+            string uri = "/username/" + Input_Username;
+            string uriclient = "{Your own directory/domain}";
+            URL = uriclient + uri;
+            var client = new RestClient(uriclient);
+            var request = new RestRequest(uri, Method.GET);
+            var response = client.Execute(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Validated = true;
+                string rawResponse = response.Content;
+                string buffer = JsonConvert.DeserializeObject<User>(response.Content).ToString();
+                UserResult = JsonConvert.DeserializeObject<User>(response.Content);
+            }
+
+
+
+        }
+    }
+}
---
+++ Market_Watch_Solo/Market_Watch_Solo/Market_Watch_Solo/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Market_Watch_Solo.Action;
+
+namespace Market_Watch_Solo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+         
+
+            if (Username.Text == null || Password.Text == null)
+            {
+                DisplayAlert("Empty Values", "Please Fill in all the the entries", "Return");
+            } else
+            {
+                var Login_Result = new Login_Check(Username.Text, Password.Text);
+                if (Login_Result.Validated)
+                {
+                    var checkoutput = Username.Text + " | " + Password.Text;
+
+                    DisplayAlert("Success", "Credentials are : " + checkoutput, "return");
+
+                } else
+                {
+                    DisplayAlert("Incorrect Credentials", Login_Result.URL, "Return");
+                }
+            }
+
+        }
+    }
+}
